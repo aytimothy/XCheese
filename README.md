a2d0d3b874688a8764da21f3af8089f0f9af66ed
Timothy Chew <wentimothychewyu@gmail.com>
2023-09-11 14:03:53 +1000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# XCheese
+
+Quick and dirty way to call `StageUpdate.ShowStageRewardUI()` to cheese and automatically clear any stage by pressing F5.  
+(For XDiVE offline version)
+
+This is moreorless a "Hello World" I wrote.
+
+## Known Bugs
+
+* Hard lock if you try to show the reward UI before the stage has begun (the "Ready, Go!" sequence has finished playing).
+ 
+Won't be bothered fixing any of them; wait for @darkhunter_'s release on the Discord.
+
+## Building
+
+Don't forget to add BepInEx to your NuGet repositories:
+
+[![Add https://nuget.bepinex.dev/v3/index.json to your NuGet packages repository list](https://i.imgur.com/THZuC8o.png)](https://i.imgur.com/THZuC8o.png)
+
+[![Add BepInEx.* to your package mappings](https://i.imgur.com/RdfBOgT.png)](https://i.imgur.com/RdfBOgT.png)
---
+++ XCheese/Plugin.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using BepInEx.Unity.IL2CPP.UnityEngine;
+using Il2CppInterop.Runtime.Injection;
+using Il2CppSystem.Collections;
+using StageLib;
+using UnityEngine;
+using KeyCode = BepInEx.Unity.IL2CPP.UnityEngine.KeyCode;
+using Object = UnityEngine.Object;
+using IEnumerator = System.Collections.IEnumerator;
+using IEnumerable = System.Collections.IEnumerable;
+
+namespace XCheese;
+
+public static class PluginInfo {
+    public const string PLUGIN_GUID = "xyz.aytimothy.bepinex.tw.com.capcom.rxd.xcheese";
+    public const string PLUGIN_NAME = "XCheese";
+    public const string PLUGIN_VERSION = "1.0";
+}
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BasePlugin {
+    public override void Load() {
+        try {
+
+            // Initialize
+            XCheese.Initialize();
+
+            // Plugin startup logic
+            Log.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+        }
+        catch (Exception ex) {
+            Log.LogFatal($"Plugin {MyPluginInfo.PLUGIN_GUID} failed to load!");
+            Log.LogError(ex);
+        }
+    }
+
+    IEnumerable DoInitialization() {
+        yield return new WaitForSeconds(3f);
+
+        
+    }
+}
+
+public class XCheese : MonoBehaviour {
+    public static XCheese Instance;
+
+    public static void Initialize() {
+        try {
+            // Register our custom types
+            ClassInjector.RegisterTypeInIl2Cpp<XCheese>();
+
+            // Do actual initialization
+            GameObject newGameObject = new GameObject("XCheese");
+            DontDestroyOnLoad(newGameObject);
+            newGameObject.hideFlags = HideFlags.HideAndDontSave;
+            Instance = newGameObject.AddComponent<XCheese>();
+            Debug.Log("Initialized XCheese!");
+        }
+        catch (Exception ex) {
+            Debug.LogException(new Il2CppSystem.Exception(ex.Message));
+            Debug.LogError("Failed to initialize XCheese (in Unity hook)");
+        }
+
+    }
+
+    void Update() {
+        if (Input.GetKeyInt(KeyCode.F5) && Event.current.type == EventType.KeyDown) {
+            DoCheese();
+        }
+    }
+
+    public void DoCheese() {
+        StageUpdate stageUpdaterObject = FindObjectOfType<StageUpdate>();
+
+        if (stageUpdaterObject != null) {
+            stageUpdaterObject.ShowStageRewardUI();
+        }
+        else {
+            Debug.LogError("You are not in a stage!!!");
+        }
+    }
+}
